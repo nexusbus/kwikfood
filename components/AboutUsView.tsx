7e2bfca9e4f4d0b363196ea7e0e0187796918544
jossa <jossa@example.com>
2026-03-19 13:55:10 +0100

    style: change about us return button text

--- components/AboutUsView.tsx
+++ components/AboutUsView.tsx
@@ -51,7 +51,7 @@ const AboutUsView: React.FC<AboutUsViewProps> = ({ onBack }) => {
             onClick={onBack}
             className="px-6 py-2 bg-primary rounded-2xl text-[11px] font-black text-white uppercase tracking-widest hover:opacity-90 transition-all shadow-lg shadow-primary/20 active:scale-95"
           >
-            Voltar para Início
+            Faça já um pedido
           </button>
         </div>
       </nav>
